ee242495ff54b3455cb3b2baab1422651ea9f97a
Davide Preti <davide.preti86@gmail.com>
2020-08-26 15:58:55 +0200

    Aggiunto ciclo for e altra prova nel bonus

--- bonus/js/bonus.js
+++ bonus/js/bonus.js
@@ -1,5 +1,7 @@
-// creo una funzione che stampi a video un risultato diverso a seconda se il numero è un multiplo di 3, di 5 o di entrambi
+var startTime, endTime;
 
+
+// creo una funzione che stampi a video un risultato diverso a seconda se il numero è un multiplo di 3, di 5 o di entrambi
 function FizzBuzz(i) {
     if (i % 3 == 0 && i % 5 == 0) {
         return console.log(`FizzBuzz (${i})`);
@@ -15,12 +17,22 @@ function FizzBuzz(i) {
 
 
 // ciclo la funzione per 100 volte
-let i = 1;
+var i = 1;
+
+// salvo il tempo iniziale prima dell'esecuzione del ciclo
+startTime = performance.now();
 
 while (i <= 100) {
     FizzBuzz(i);
     i++;
 }
+
+// salvo il tempo finale dopo l'esecuzione del ciclo
+endTime = performance.now();
+
+// stampa a video il tempo di esecuzione della funzione
+var run1 = endTime - startTime;
+console.log(run1);
 
 /* ----------
     test
@@ -45,7 +57,85 @@ function FizzBuzz2(i) {
 
 i = 1;
 
+startTime = performance.now();
+
 while (i <= 100) {
     FizzBuzz2(i);
     i++;
 }
+
+endTime = performance.now();
+var run2 = endTime - startTime;
+console.log(run2);
+
+
+/* ----------
+    test2
+ -----------*/
+
+function FizzBuzz3(i) {
+
+    if (i % 3 != 0 && i % 5 != 0) {
+        return console.log(i);
+    }
+
+    if (i % 3 == 0) {
+        if (i % 5 == 0) {
+            return console.log(`FizzBuzz (${i})`);
+        }
+        return console.log(`Fizz (${i})`);
+    }
+
+    return console.log(`Buzz (${i})`);
+}
+
+
+i = 1;
+
+startTime = performance.now();
+
+while (i <= 100) {
+    FizzBuzz3(i);
+    i++;
+}
+
+endTime = performance.now();
+var run3 = endTime - startTime;
+console.log(run3);
+
+
+/* ----------
+    test3
+ -----------*/
+
+function FizzBuzz4(i) {
+
+    if (i % 3 != 0 && i % 5 != 0) {
+        return console.log(i);
+    }
+
+    if (i % 5 == 0) {
+        if (i % 3 == 0) {
+            return console.log(`FizzBuzz (${i})`);
+        }
+        return console.log(`Buzz (${i})`);
+    }
+
+    return console.log(`Fizz (${i})`);
+}
+
+
+i = 1;
+
+startTime = performance.now();
+
+while (i <= 100) {
+    FizzBuzz4(i);
+    i++;
+}
+
+endTime = performance.now();
+var run4 = endTime - startTime;
+console.log(run4);
+
+console.log(run1 + " " + run2 + " " + run3 + " " + run4);
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let i = 1;
+var i = 1;
 
 while (i <= 100) {
 
@@ -13,8 +13,19 @@ while (i <= 100) {
     }
 
     i++;
-
 }
 
+console.log("*** Inizio ciclo for ***")
 
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+        console.log(`FizzBuzz (${index})`);
+    } else if (index % 3 == 0) {
+        console.log(`Fizz (${index})`);
+    } else if (index % 5 == 0) {
+        console.log(`Buzz (${index})`);
+    } else {
+        console.log(index);
+    }
 
+}
